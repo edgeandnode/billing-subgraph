9b48b124bc9c989707f4aba949ac7859c8cdbc9d
David Kajpust <kajpustd@gmail.com>
2021-06-10 15:12:19 -0400

    Complete mumbai subgraph and deploy to hosted

---
+++ src/mappings/billing.ts
@@ -0,0 +1,108 @@
+import { Billing, User, TokensAdded, TokensRemoved, TokensPulled } from '../types/schema'
+import {
+  TokensAdded as AddedEvent,
+  TokensRemoved as RemovedEvent,
+  TokensPulled as PulledEvent,
+  GatewayUpdated,
+  NewOwnership,
+  Billing as BillingContract,
+} from '../types/Billing/Billing'
+import { BigInt, Address } from '@graphprotocol/graph-ts'
+
+/**
+ * @dev handleEpochRun - Sets the gateway on the Billing Entity. Creates entity on first try
+ */
+export function handleGatewayUpdated(event: GatewayUpdated): void {
+  let billing = Billing.load('1')
+  if (billing == null) {
+    billing = new Billing('1')
+    let contract = BillingContract.bind(event.address)
+    billing.governor = contract.governor()
+  }
+  billing.gateway = event.params.newGateway
+  billing.save()
+}
+
+/**
+ * @dev Sets the governor on the Billing Entity
+ */
+export function handleNewOwnership(event: NewOwnership): void {
+  let billing = Billing.load('1')
+  billing.governor = event.params.to
+  billing.save()
+}
+
+/**
+ * @dev Handle the Tokens Added event
+ */
+export function handleTokensAdded(event: AddedEvent): void {
+  let user = createOrLoadUser(event.params.user)
+  user.billingBalance = user.billingBalance.plus(event.params.amount)
+  user.save()
+
+  let tx = new TokensAdded(
+    event.transaction.hash.toHexString().concat(event.transactionLogIndex.toString()),
+  )
+  tx.hash = event.transaction.hash
+  tx.blockNumber = event.block.number.toI32()
+  tx.timestamp = event.block.timestamp.toI32()
+  tx.user = event.params.user.toHexString()
+  tx.amount = event.params.amount
+  tx.type = 'TokensAdded'
+  tx.save()
+}
+
+/**
+ * @dev Handle the Tokens Removed event
+ */
+export function handleTokensRemoved(event: RemovedEvent): void {
+  let user = createOrLoadUser(event.params.user)
+  user.billingBalance = user.billingBalance.minus(event.params.amount)
+  user.save()
+
+  let tx = new TokensRemoved(
+    event.transaction.hash.toHexString().concat(event.transactionLogIndex.toString()),
+  )
+  tx.hash = event.transaction.hash
+  tx.blockNumber = event.block.number.toI32()
+  tx.timestamp = event.block.timestamp.toI32()
+  tx.user = event.params.user.toHexString()
+  tx.amount = event.params.amount
+  tx.type = 'TokensRemoved'
+  tx.to = event.params.to
+  tx.save()
+}
+
+/**
+ * @dev Handle the Tokens Pulled event
+ */
+export function handleTokensPulled(event: PulledEvent): void {
+  let user = createOrLoadUser(event.params.user)
+  user.billingBalance = user.billingBalance.minus(event.params.amount)
+  user.save()
+
+  let tx = new TokensPulled(
+    event.transaction.hash.toHexString().concat(event.transactionLogIndex.toString()),
+  )
+  tx.hash = event.transaction.hash
+  tx.blockNumber = event.block.number.toI32()
+  tx.timestamp = event.block.timestamp.toI32()
+  tx.user = event.params.user.toHexString()
+  tx.amount = event.params.amount
+  tx.type = 'TokensPulled'
+  tx.save()
+}
+
+/**
+ * @dev Helper function to load or create a User
+ */
+function createOrLoadUser(userAddress: Address): User {
+  let id = userAddress.toHexString()
+  let user = User.load(id)
+  if (user == null) {
+    user = new User(id)
+    user.billingBalance = BigInt.fromI32(0)
+    user.polygonGRTBalance = BigInt.fromI32(0)
+  }
+  return user as User
+}
